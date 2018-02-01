9fafc529c03458488e3fbe643fcb654c019cf964
Jonathan Williams <jtw365@live.missouristate.edu>
2018-02-01 03:36:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
-    var lowerCase = string.toLowercase();
+    var lowerCase = string.toLowerCase();
     return ('I can\'t hear you!');
   }
 }
